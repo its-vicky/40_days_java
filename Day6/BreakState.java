623149cf9030e2223bce946e4e13d3e491ea19f4
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-01-29 00:38:24 +0530

    Add files via upload

---
+++ Day6/BreakState.java
@@ -0,0 +1,17 @@
+package fiftydayscode_bran_state;
+
+public class BreakState {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i);
+			if(i==5)
+			{
+				System.out.println("This is Break Statement..!!");
+				break;
+			}
+		}
+	}
+
+}
---
+++ Day6/ContinueState.java
@@ -0,0 +1,18 @@
+package fiftydayscode_bran_state;
+
+public class ContinueState {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Learn Java..!!  :  "+i);
+			if(i==7)
+			{
+				System.out.println("The Continue Statement..!!");
+				continue;
+			}
+		}
+
+	}
+
+}
