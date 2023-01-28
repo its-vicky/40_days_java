3520755c221ddbacf80faeadfbd02259bc36e719
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-01-29 00:45:43 +0530

    Update ContinueState.java

--- Day6/ContinueState.java
+++ Day6/ContinueState.java
@@ -1,8 +1,10 @@
 package fiftydayscode_bran_state;
 
-public class ContinueState {
+public class ContinueState 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		for(int i=0;i<10;i++)
 		{
 			System.out.println("Learn Java..!!  :  "+i);
