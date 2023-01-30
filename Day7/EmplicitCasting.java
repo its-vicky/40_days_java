9ae7e1fb61a4e32cc7b06c8e4b4d4f6106d2cd7c
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-01-30 19:57:00 +0530

    Add files via upload

---
+++ Day7/EmplicitCasting.java
@@ -0,0 +1,14 @@
+package TypeCasting;
+
+public class EmplicitCasting {
+
+	public static void main(String[] args) {
+		double a=108.87;
+		long c=(long)a;
+		int b=(int)c;
+		System.out.println("The Double is "+a);
+		System.out.println("The Long is "+c);
+		System.out.println("The Int is "+b);
+	}
+
+}
---
+++ Day7/ImpliciteCasting.java
@@ -0,0 +1,10 @@
+package TypeCasting;
+
+public class ImpliciteCasting {
+	public static void main(String[] args) {
+		int a=10;
+		float c=a;
+		System.out.println("Int is "+a);
+		System.out.println("Float is "+c);
+	}
+}
