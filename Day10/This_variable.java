f53749573b27488611f79c3e3916c0272e361abf
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-02 23:02:55 +0530

    This_variable.java
    This keyword with Only Instance Variable..

--- Day10/This_variable.java
+++ Day10/This_variable.java
@@ -1,13 +1,15 @@
 package fiftydayscode_this;
 
-public class This_variable {
+public class This_variable 
+{
 
 		int a=20;
 		public void Square(int a)
 		{
 			this.a=a;
 		}
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		This_variable tv=new This_variable();
 		System.out.println("The SQUARE of the number is "+(tv.a*tv.a));
 
