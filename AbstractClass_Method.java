1cf12fca1e8943a34eb2ac85351633287acc2e8f
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-05 21:51:26 +0530

    AbstractClass_Method.java
    The Abstract.

---
+++ AbstractClass_Method.java
@@ -0,0 +1,18 @@
+package fiftydayscode_abstract;
+
+abstract class Honda4
+{
+		abstract void run();
+}
+class AbstractClass_Method extends Honda4 
+{
+		void run()
+		{
+			System.out.println("running safely");
+		}
+		public static void main(String[] args) 
+		{
+			Honda4 obj=new AbstractClass_Method();
+			obj.run();
+		}
+}
