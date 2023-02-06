e10aee873e7ec89f2969e117dc61fa1b1a99433b
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-06 22:06:11 +0530

    ProtectedEx.java

---
+++ Day14/ProtectedEx.java
@@ -0,0 +1,11 @@
+package fiftydayscode_accessSpecifiers;
+
+public class ProtectedEx {
+	protected int a=40;
+	protected int b=45;
+	protected void msg()
+	{
+			System.out.println("The Addition of the number is "+(a+b));
+	}
+
+}
