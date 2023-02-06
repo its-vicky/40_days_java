e378a04b4b2d279b96c9dc1cda001f71fa0c9ecf
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-06 22:13:26 +0530

    PublicExample.java

---
+++ Day14/PublicExample.java
@@ -0,0 +1,12 @@
+package fiftydayscode_accessSpecifiers;
+
+public class PublicExample {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the Number for SQUARE: ");
+		PublicEx ps=new PublicEx();
+		ps.run();
+
+	}
+
+}
