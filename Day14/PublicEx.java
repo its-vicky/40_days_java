fb6e4df174604d66edce56ca7327109fb4d92565
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-06 22:09:12 +0530

    PublicEx.java

---
+++ Day14/PublicEx.java
@@ -0,0 +1,10 @@
+package fiftydayscode_accessSpecifiers;
+import java.util.Scanner;
+public class PublicEx {
+	Scanner vc=new Scanner(System.in);
+	int a=vc.nextInt();
+	public void run()
+	{
+		System.out.println("The Square of the Number is "+(a*a));
+	}
+}
