65a2be19193cd806aefec946479ba8644886c7d6
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-11 19:26:10 +0530

    StringBufferExample.java

---
+++ Day19/StringBufferExample.java
@@ -0,0 +1,12 @@
+class StringBufferExample
+{
+	public static void main(String args[])
+	{
+		StringBuffer sb=new StringBuffer("India ");
+		System.out.println("The StringBuffer capacity : "+sb.capacity());
+		System.out.println("The append the string is : "+sb.append("Jay Ho "));
+		System.out.println("The string delete is : "+sb.delete(1,3));
+		System.out.println("The Reverse String is : "+sb.reverse());
+		System.out.println("The Replace the String is : "+sb.replace(1,3, ("Welcome")));
+	}
+}
