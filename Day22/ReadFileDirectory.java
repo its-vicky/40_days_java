5e4022c2cf04c022dfb241588fe2959b8a647289
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-14 20:24:15 +0530

    ReadFileDirectory.java

--- Day22/ReadFileDirectory.java
+++ Day22/ReadFileDirectory.java
@@ -1,23 +1,27 @@
 package fiftydayscode_CreateDirectory;
+
 import java.io.File; 
-//Importing FileNotFoundException class for handling errors
 import java.io.FileNotFoundException; 
-//Importing the Scanner class for reading text files
 import java.util.Scanner; 
-public class ReadFileDirectory {
-	public static void main(String[] args) {
-		 try {
-		 // Create f1 object of the file to read data
-		 File f1 = new File("D:FileOperationExample.txt"); 
-		 Scanner dataReader = new Scanner(f1);
-		 while (dataReader.hasNextLine()) {
-			 String fileData = dataReader.nextLine();
-			 System.out.println(fileData);
-			 }
-			 dataReader.close();
-			 } catch (FileNotFoundException exception) {
+
+public class ReadFileDirectory
+{
+	public static void main(String[] args)
+	{
+		 try
+		 {
+			File f1 = new File("D:FileOperationExample.txt"); 
+		 	Scanner dataReader = new Scanner(f1);
+			while (dataReader.hasNextLine()) 
+			{
+				String fileData = dataReader.nextLine();
+				System.out.println(fileData);
+			}
+			dataReader.close();
+		 } catch (FileNotFoundException exception) 
+		 {
 			 System.out.println("Unexcpected error occurred!");
 			 exception.printStackTrace();
-			 }
 		 }
+	}
 }
