ff85d398de460b92ce14a2134aeb62bfa8ee2a27
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-15 23:47:24 +0530

    DateExam.java

---
+++ Day23/DateExam.java
@@ -0,0 +1,10 @@
+package fiftydayscode_TimeDate;
+import java.time.LocalDate;
+public class DateExam {
+	
+		public static void main(String args[])
+		{
+			LocalDate ld=LocalDate.now();
+			System.out.println("The Today's Date is "+ld);
+		}
+}
