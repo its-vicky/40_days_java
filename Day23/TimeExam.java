15e341400e5ca77974083d4ba0f1273470a02ca0
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-15 23:49:15 +0530

    TimeExam.java

---
+++ Day23/TimeExam.java
@@ -0,0 +1,11 @@
+package fiftydayscode_TimeDate;
+import java.time.LocalTime;
+public class TimeExam {
+
+	public static void main(String[] args) {
+		LocalTime lt=LocalTime.now();
+		System.out.println("The Current Time is "+lt);
+
+	}
+
+}
