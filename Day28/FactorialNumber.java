be216c0f8911458231ba9c03ddc6bcfe8214d47a
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-20 22:11:04 +0530

    FactorialNumber.java
    Finding the factorial of the given number using the java.

---
+++ Day28/FactorialNumber.java
@@ -0,0 +1,17 @@
+package fiftydayscode_practice_day2;
+import java.util.Scanner;
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		Scanner vc=new Scanner(System.in);
+		System.out.print("Enter the number for calculating factorial : ");
+		int a=vc.nextInt();
+		int fact=1;
+		for(int i=1;i<=a;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println("Factorial of "+a+" is "+fact);
+	}
+
+}
