9bb9ffa1c2ce1cf3ff429c77567bc142a32af0e6
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-23 23:09:56 +0530

    SquareRoot.java
    Finding the square root in java

---
+++ Day31/SquareRoot.java
@@ -0,0 +1,12 @@
+import java.lang.Math.*;
+import java.util.*;
+class SquareRoot
+{
+	public static void main(String args[])
+	{	
+		Scanner vc=new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int no=vc.nextInt();
+		System.out.println("The Square root is "+Math.sqrt(no));
+	}
+}
