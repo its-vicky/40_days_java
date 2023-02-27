02d5bb95c2384ed16c02b32849cada1992cefe90
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-27 23:05:00 +0530

    VideoLauncher.java

--- Day35/VideoLauncher.java
+++ Day35/VideoLauncher.java
@@ -1,6 +1,7 @@
 package Project2;
 import java.util.Scanner;
-public class VideoLauncher {
+public class VideoLauncher 
+{
 	public static void main(String[] args)
 	{
 		Scanner vc=new Scanner(System.in);
