160e2b3391b0ac68bb3e8f11116a66068eeb06b3
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-02-28 23:22:29 +0530

    Student.java

---
+++ Day36/mile1/bean/Student.java
@@ -0,0 +1,36 @@
+package com.mile1.bean;
+
+public class Student 
+{
+	private String name;
+	private int marks[];
+	private String grade;
+	public Student()
+	{
+
+	}
+	public Student(String name, int[] marks, String grade) 
+	{
+		this.name = name;
+		this.marks = marks;
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int[] getMarks() {
+		return marks;
+	}
+	public void setMarks(int[] marks) {
+		this.marks = marks;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+}
