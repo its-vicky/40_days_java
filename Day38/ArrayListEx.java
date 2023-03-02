e95bd09219868318e628978db12a485771531786
Vikas Choudhari <123319826+its-vicky@users.noreply.github.com>
2023-03-02 23:19:10 +0530

    ArrayListEx.java

---
+++ Day38/ArrayListEx.java
@@ -0,0 +1,16 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArraListEx {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		List list=new ArrayList();
+		list.add(a);
+		
+
+	}
+
+}
